bd7eb06d3aeff34d2d2666c1e0a37f37a98741a1
Filipe Dias <filipediascontato@gmail.com>
2022-11-20 18:22:34 -0300

    Starts to implements the hero section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,37 @@
-import { HomeContainer } from './styles'
+import {
+  BackgroundContainer,
+  HeroSection,
+  HomeContainer,
+  HeroContent,
+  TitleContainer,
+} from './styles'
+import HeroImage from '../../assets/hero.png'
 
 export function Home() {
   return (
     <HomeContainer>
-      <h1>Home</h1>
+      <HeroSection>
+        <BackgroundContainer />
+
+        <main>
+          <HeroContent>
+            <TitleContainer>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+              <p>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </p>
+            </TitleContainer>
+
+            <div></div>
+          </HeroContent>
+
+          <img
+            src={HeroImage}
+            alt="Image de um copo de café com a logo do coffee delivery rodeado por grãos de café"
+          />
+        </main>
+      </HeroSection>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,67 @@
 import styled from 'styled-components'
 
+import BackgroundImage from '../../assets/background.svg'
+
 export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+export const HeroSection = styled.section`
+  position: relative;
+  width: 100vw;
+  height: 34rem;
+
+  main {
+    display: flex;
+    align-items: center;
+    gap: 3.5rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    max-width: 90rem;
+    margin: auto;
+    padding: 5.75rem 10rem;
+  }
+`
+
+export const BackgroundContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${BackgroundImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  filter: blur(80px);
+`
+
+export const HeroContent = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 4.125rem;
+`
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 1rem;
+
+  h1 {
+    font-family: 'Baloo 2';
+    font-weight: 800;
+    font-size: 3rem;
+    line-height: 3.875rem;
+    color: ${(props) => props.theme.colors['gray-900']};
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 1.625rem;
+    color: ${(props) => props.theme.colors['gray-800']};
+  }
 `
