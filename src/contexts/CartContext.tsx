8184ebe47279e6b4df5da71b2d579f2ba6e74128
Filipe Dias <filipediascontato@gmail.com>
2022-11-30 22:24:54 -0300

    Starts to implements CartContext state and functions

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,7 +1,16 @@
 import { createContext, ReactNode } from 'react'
 
+import { usePersistedState } from '../hooks/usePersistedState'
+
+interface CartItem {
+  id: string
+  amount: number
+}
+
 interface CartContextData {
+  cartItems: CartItem[]
   itemsAmount: number
+  addItemToCart: (id: string, amount: number) => void
 }
 
 export const CartContext = createContext({} as CartContextData)
@@ -11,8 +20,38 @@ interface CartContextProviderProps {
 }
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = usePersistedState<CartItem[]>(
+    '@coffee-delivery:cart-state-1.0.0',
+    [],
+  )
+
+  const itemsAmount = cartItems.reduce(
+    (accumulator, currentItem) => accumulator + currentItem.amount,
+    0,
+  )
+
+  function addItemToCart(id: string, amount: number) {
+    const itemAlreadyExists = cartItems.find((item) => item.id === id)
+
+    if (itemAlreadyExists) {
+      const newCartItems = cartItems.map((item) => {
+        if (item.id === id) {
+          const incrementedItemAmount = item.amount + amount
+          return { ...item, amount: incrementedItemAmount }
+        }
+
+        return item
+      })
+
+      setCartItems(newCartItems)
+    } else {
+      const itemToAdd = { id, amount }
+      setCartItems((state) => [...state, itemToAdd])
+    }
+  }
+
   return (
-    <CartContext.Provider value={{ itemsAmount: 0 }}>
+    <CartContext.Provider value={{ cartItems, itemsAmount, addItemToCart }}>
       {children}
     </CartContext.Provider>
   )
