f483f8cc678302e2a820a109105a782a5e15b19e
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-12-07 13:24:55 -0300

    Adds form register to PaymentMethodForm component

--- src/pages/Checkout/components/PaymentMethodForm/index.tsx
+++ src/pages/Checkout/components/PaymentMethodForm/index.tsx
@@ -1,25 +1,33 @@
-import { PaymentFormContainer } from './styles'
+import { useFormContext } from 'react-hook-form'
 import { PaymentMethodButton } from '../PaymentMethodButton'
 import { Bank, CreditCard, Money } from 'phosphor-react'
 
 export function PaymentMethodForm() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<ConfirmOrderFormData>()
+
   return (
     <PaymentFormContainer>
-      <PaymentMethodButton
-        id="credit"
-        icon={<CreditCard size={16} />}
-        label="Cartão de Crédito"
-      />
-      <PaymentMethodButton
-        id="debit"
-        icon={<Bank size={16} />}
-        label="Cartão de Débito"
-      />
-      <PaymentMethodButton
-        id="money"
-        icon={<Money size={16} />}
-        label="Dinheiro"
-      />
+        <PaymentMethodButton
+          id="credit"
+          icon={<CreditCard size={16} />}
+          label="Cartão de Crédito"
+          {...register('paymentMethod')}
+        />
+        <PaymentMethodButton
+          id="debit"
+          icon={<Bank size={16} />}
+          label="Cartão de Débito"
+          {...register('paymentMethod')}
+        />
+        <PaymentMethodButton
+          id="money"
+          icon={<Money size={16} />}
+          label="Dinheiro"
+          {...register('paymentMethod')}
+        />
     </PaymentFormContainer>
   )
 }
